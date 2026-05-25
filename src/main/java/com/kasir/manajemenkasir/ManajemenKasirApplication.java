26df78e300540ab20894b5c70217559fd53f50b7
omrndv <umarnadiv@gmail.com>
2026-05-25 11:08:33 +0700

    Initial commit project Spring Boot manajemen kasir

---
+++ src/main/java/com/kasir/manajemenkasir/ManajemenKasirApplication.java
@@ -0,0 +1,13 @@
+package com.kasir.manajemenkasir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManajemenKasirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManajemenKasirApplication.class, args);
+	}
+
+}
