71a48695cdc84f00f16f71e7c54fbe28508f0724
Xoltvez <faizazzahra88@gmail.com>
2026-06-14 11:37:28 +0700

    Update

--- src/main/java/com/kasir/manajemenkasir/controller/LoginController.java
+++ src/main/java/com/kasir/manajemenkasir/controller/LoginController.java
@@ -1,17 +1,22 @@
 package com.kasir.manajemenkasir.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.kasir.manajemenkasir.model.User;
 import com.kasir.manajemenkasir.service.BarangService;
 import com.kasir.manajemenkasir.service.LaporanService;
 import com.kasir.manajemenkasir.service.TransaksiService;
 import com.kasir.manajemenkasir.service.UserService;
+
 import jakarta.servlet.http.HttpSession;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class LoginController {
+
     private final UserService userService;
     private final BarangService barangService;
     private final TransaksiService transaksiService;
@@ -30,6 +35,11 @@ public class LoginController {
     }
 
     @GetMapping("/")
+    public String landingPage() {
+        return "landingpage";
+    }
+
+    @GetMapping("/login")
     public String loginPage() {
         return "login";
     }
